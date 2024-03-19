6d00800963efe6f6e5c5f234131ff6a53a661f4d
pellekrab <78560773+PelleKrab@users.noreply.github.com>
2024-03-19 14:32:50 -0600

    Private message prototype complete

--- server.cpp
+++ server.cpp
@@ -7,27 +7,55 @@
 #include <map>
 
 std::map<int, int> client_sockets;
-int next_id = 1; 
+int next_id = 1;
 
-void handle_client(int client_socket) {
+void handle_client(int client_socket)
+{
     char buffer[1024] = {0};
     int client_id = client_sockets[client_socket];
 
-    while (true) {
+    while (true)
+    {
         memset(buffer, 0, sizeof(buffer));
         int valread = read(client_socket, buffer, 1024);
-        if (valread <= 0) {
+        if (valread <= 0)
+        {
             break;
         }
 
-        // Prepend the message with the sender's ID
-        std::string message = "Client " + std::to_string(client_id) + ": " + buffer;
+        if (strncmp(buffer, "-p ", 3) == 0)
+        {
+            std::string command(buffer);
+            size_t spacePos = command.find(' ', 3);
+            if (spacePos != std::string::npos)
+            {
+                std::string recieverID = command.substr(3, spacePos - 3);
+                std::string message = command.substr(spacePos+1);
+                for (auto &pair : client_sockets)
+                {
+                    int other_socket = pair.first;
+                    int other_client_id = pair.second;
+                    if (other_client_id == stoi(recieverID)) 
+                    {
+                        send(other_socket, message.c_str(), message.size(), 0);
+                        break; 
+                    }
+                }
+            }
+        }
+        else
+        {
+            // Prepend the message with the sender's ID
+            std::string message = "Client " + std::to_string(client_id) + ": " + buffer;
 
-        // Relay the message to all other clients
-        for (auto& pair : client_sockets) {
-            int other_socket = pair.first;
-            if (other_socket != client_socket) {
-                send(other_socket, message.c_str(), message.size(), 0);
+            // Relay the message to all other clients
+            for (auto &pair : client_sockets)
+            {
+                int other_socket = pair.first;
+                if (other_socket != client_socket)
+                {
+                    send(other_socket, message.c_str(), message.size(), 0);
+                }
             }
         }
     }
@@ -35,18 +63,21 @@ void handle_client(int client_socket) {
     close(client_socket);
 }
 
-int main() {
+int main()
+{
     int server_fd;
     struct sockaddr_in address;
     int opt = 1;
     int addrlen = sizeof(address);
 
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
 
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
+    {
         perror("setsockopt");
         exit(EXIT_FAILURE);
     }
@@ -55,19 +86,23 @@ int main() {
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(8080);
 
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
+    {
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
 
-    if (listen(server_fd, 3) < 0) {
+    if (listen(server_fd, 3) < 0)
+    {
         perror("listen");
         exit(EXIT_FAILURE);
     }
 
-    while (true) {
+    while (true)
+    {
         int new_socket;
-        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0)
+        {
             perror("accept");
             exit(EXIT_FAILURE);
         }
