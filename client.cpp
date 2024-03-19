c808c5de6edd43a9b1504cf123224df14d142ffd
pellekrab <78560773+PelleKrab@users.noreply.github.com>
2024-03-19 12:00:32 -0600

    Added message receiving in a separate thread

--- client.cpp
+++ client.cpp
@@ -1,16 +1,35 @@
-#include <sys/socket.h>     
-#include <arpa/inet.h>      
-#include <unistd.h>         
-#include <cstring>          
-#include <iostream>         
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <cstring>
+#include <iostream>
+#include <thread>
 
-int main() {
-    struct sockaddr_in serv_addr;   
+void receiveMessages(int sock) {
+    char buffer[1024] = {0};
+
+    while (true) {
+        memset(buffer, 0, sizeof(buffer));
+        int valread = read(sock, buffer, 1024);
+        if (valread <= 0) {
+            break;
+        }
+
+        std::cout << "\r" << std::string(80, ' ') << "\r";
+        std::cout << buffer << std::endl;
+        std::cout << "You: " << std::flush;
+    }
+}
+
+int main()
+{
+    struct sockaddr_in serv_addr;
     int sock = 0;
     char buffer[1024] = {0};
     std::string hello = "Hello from client";
 
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
         std::cout << "\n Socket creation error \n";
         return -1;
     }
@@ -18,31 +37,37 @@ int main() {
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(8080);
 
-    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0)
+    {
         std::cout << "\nInvalid address/ Address not supported \n";
         return -1;
     }
 
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
         std::cout << "\nConnection Failed \n";
         return -1;
     }
 
-    while( true ) {
+
+    std::thread receiveThread(receiveMessages, sock);
+    receiveThread.detach();
+
+    while (true)
+    {
         std::string message;
-        std::cout << "Enter message (to exit type 'exit'): ";
+        std::cout << "You: ";
         std::getline(std::cin, message);
 
-        if (message == "exit") {
+        if (message == "exit")
+        {
             break;
         }
 
         send(sock, message.c_str(), message.size(), 0);
-        std::cout << "Message sent\n";
+        
 
-        read(sock, buffer, 1024);
-        std::cout << buffer << std::endl;
     }
-   
+
     return 0;
 }
